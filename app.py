82e02c406d849a5941384a6c45971bd27a474379
Atsushi Fukuda <kyomu1@gmail.com>
2019-03-14 17:42:26 +0900

    データベース（書き込み）の中身を表示できるようにした。

--- app.py
+++ app.py
@@ -2,12 +2,37 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import csv
-
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+# データベースの立ち上げ
+db_uri = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
+
+# 書き込み内容のDB
+class Article(db.Model):
+    # 内容はプライマイキー、投稿日、名前、本文
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    name = db.Column(db.Text())
+    msg = db.Column(db.Text())
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        date = datetime.now()
+        name = request.form['name']
+        msg = request.form['msg']
+        admin = Article(pub_date=date, name=name, msg=msg)
+        db.session.add(admin)
+        db.session.commit()
+        contents = Article.query.all()
+        return render_template('index.html', lines = contents)
+
+    contents = Article.query.all()
+    print(contents)
+    return render_template('index.html', contents=contents)
+    
 
 if __name__ == '__main__':
     app.run()
